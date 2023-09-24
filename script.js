d19d184ff445f84899fe1228a0b3ba7de0363fee
bourawi12 <mohamed12bouaziz@gmail.com>
2023-09-25 00:36:31 +0100

    doing the same thing for the other buttons

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-function fonction(){
+function fonction1(){
 document.getElementById('button1').src ="pic2.jpg";
-
 }
+function fonction2(){
+    document.getElementById('button2').src ="pic2.jpg";
+    }
+    function fonction3(){
+        document.getElementById('button3').src ="pic2.jpg";
+        }
+        function fonction4(){
+            document.getElementById('button4').src ="pic2.jpg";
+            }
+            function fonction5(){
+                document.getElementById('button5').src ="pic2.jpg";
+                }
+                function fonction6(){
+                    document.getElementById('button6').src ="pic2.jpg";
+                    }
+                    function fonction7(){
+                        document.getElementById('button7').src ="pic2.jpg";
+                        }
+                        function fonction8(){
+                            document.getElementById('button8').src ="pic2.jpg";
+                            }
+                            function fonction9(){
+                                document.getElementById('button9').src ="pic2.jpg";
+                                }
